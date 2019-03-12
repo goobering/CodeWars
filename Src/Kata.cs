7c327b6588c53b38afed99f2ced68f8366dd530f
Dale Johnston <goobering@gmail.com>
2019-03-12 14:23:42 +0000

    Tests passing

--- Src/Kata.cs
+++ Src/Kata.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Src
 {
@@ -6,7 +7,39 @@ namespace Src
     {
         public static long PowerSumDigTerm(int n) 
         {
-            return -1;
+            List<long> a = new List<long>();
+             
+            for (int b = 2; b < 400; b++) {
+                long value = b;
+                for (int e = 2; e < 50; e++) {
+                    value *= b;
+            
+                    if (DigitSum(value) == b) {
+                        a.Add(value);            
+                    }
+                    if (a.Count > 50) break;                    
+                }
+                if (a.Count > 50) break;                    
+            }
+
+            a.Sort();
+
+            foreach(var v in a)
+            {
+                Console.WriteLine(v);
+            }
+            return a[n - 1];
+        }
+
+        private static long DigitSum(long n) 
+        {
+            long sum = 0;
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+
+            return sum;
         }
     }
 }
