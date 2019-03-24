6bdcdbe27ad06234076c25d0af69ac474a75461f
Dale Johnston <goobering@gmail.com>
2019-03-24 01:02:58 +0000

    Tests passing

--- Src/Kata.cs
+++ Src/Kata.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Src
 {
@@ -6,7 +8,33 @@ namespace Src
     {
         public static int Score(int[] dice) 
         {
-            throw new NotImplementedException();
+            var score = 0;
+            var scoreTable = new Dictionary<int, int> {
+                {1, 1000},
+                {6, 600},
+                {5, 500},
+                {4, 400},
+                {3, 300},
+                {2, 200}
+            };
+
+            for(int i = 1; i < 7; i++)
+            {
+                if(dice.Count(d => d == i) >= 3)
+                {
+                    score += scoreTable[i];
+
+                    for(int j = 0; j < 3; j++)
+                    {
+                        dice[Array.IndexOf(dice, i)] = 0;
+                    }
+                }
+            }
+
+            score += dice.Count(d => d == 1) * 100;
+            score += dice.Count(d => d == 5) * 50;
+
+            return score;
         }
     }
 }
