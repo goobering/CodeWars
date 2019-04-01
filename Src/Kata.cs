37f44747bc09e78ba02a0cfddd897ba99069ed72
Dale Johnston <goobering@gmail.com>
2019-04-01 10:39:32 +0100

    Tests passing

--- Src/Kata.cs
+++ Src/Kata.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Src
 {
@@ -6,6 +7,49 @@ namespace Src
     {
         public static int IsSolved(int[,] board)
         {
+            // Check win on rows
+            for(int i = 0; i < board.GetLength(0); i++)
+            {
+                if(((board[i, 0] == board[i, 1]) && (board[i, 0] == board[i, 2])) && board[i, 0] != 0)
+                {
+                    return board[i, 0];
+                }
+            }
+
+            // Check win on columns
+            for(int i = 0; i < board.GetLength(1); i++)
+            {
+                if(((board[0, i] == board[1, i]) && (board[0, i] == board[2, i])) && board[0, i] != 0)
+                {
+                    return board[0, i];
+                }
+            }
+
+            // Check win on down diagonal
+            if(((board[0, 0] == board[1, 1]) && (board[0, 0] == board[2, 2])) && board[0, 0] != 0)
+            {
+                return board[0, 0];
+            }
+
+            // Check win on up diagonal
+            if(((board[0, 2] == board[1, 1]) && (board[0, 2] == board[2, 0])) && board[0, 2] != 0)
+            {
+                return board[0, 2];
+            }
+
+            // Check incomplete board
+            for(int i = 0; i < board.GetLength(0); i++)
+            {
+                for(int j = 0; j < board.GetLength(1); j++)
+                {
+                    if(board[i, j] == 0)
+                    {
+                        return -1;
+                    };
+                }
+            }
+
+            // Game's a draw
             return 0;
         }
     }
--- Tests/TestKata.cs
+++ Tests/TestKata.cs
@@ -12,7 +12,7 @@ namespace Tests
         }
 
         [Test]
-        public void test1() 
+        public void testXWinOnFirstRow() 
         {
             int[,] board = new int[,] 
             {
@@ -22,6 +22,279 @@ namespace Tests
             };
              
             Assert.AreEqual(1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testXWinOnSecondRow() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 0, 2, 2 }, 
+                { 1, 1, 1 }, 
+                { 0, 0, 0 } 
+            };
+             
+            Assert.AreEqual(1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testXWinOnThirdRow() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 0, 2, 2 }, 
+                { 0, 0, 0 }, 
+                { 1, 1, 1 } 
+            };
+             
+            Assert.AreEqual(1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testXWinOnFirstColumn() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 1, 2, 2 }, 
+                { 1, 0, 0 }, 
+                { 1, 0, 0 } 
+            };
+             
+            Assert.AreEqual(1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testXWinOnSecondColumn() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 2, 1, 2 }, 
+                { 0, 1, 0 }, 
+                { 0, 1, 0 } 
+            };
+             
+            Assert.AreEqual(1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testXWinOnThirdColumn() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 2, 2, 1 }, 
+                { 0, 0, 1 }, 
+                { 0, 0, 1 } 
+            };
+             
+            Assert.AreEqual(1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testXWinOnDownDiagonal() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 1, 2, 2 }, 
+                { 0, 1, 0 }, 
+                { 0, 0, 1 } 
+            };
+             
+            Assert.AreEqual(1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testXWinOnUpDiagonal() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 2, 2, 1 }, 
+                { 0, 1, 0 }, 
+                { 1, 0, 0 } 
+            };
+             
+            Assert.AreEqual(1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testOWinOnFirstRow() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 2, 2, 2 }, 
+                { 0, 1, 1 }, 
+                { 0, 0, 0 } 
+            };
+             
+            Assert.AreEqual(2, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testOWinOnSecondRow() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 0, 1, 1 }, 
+                { 2, 2, 2 }, 
+                { 0, 0, 0 } 
+            };
+             
+            Assert.AreEqual(2, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testOWinOnThirdRow() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 0, 1, 1 }, 
+                { 0, 0, 0 }, 
+                { 2, 2, 2 } 
+            };
+             
+            Assert.AreEqual(2, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testOWinOnFirstColumn() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 2, 1, 1 }, 
+                { 2, 0, 0 }, 
+                { 2, 0, 0 } 
+            };
+             
+            Assert.AreEqual(2, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testOWinOnSecondColumn() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 1, 2, 1 }, 
+                { 0, 2, 0 }, 
+                { 0, 2, 0 } 
+            };
+             
+            Assert.AreEqual(2, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testOWinOnThirdColumn() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 1, 1, 2 }, 
+                { 0, 0, 2 }, 
+                { 0, 0, 2 } 
+            };
+             
+            Assert.AreEqual(2, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testOWinOnDownDiagonal() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 2, 1, 1 }, 
+                { 0, 2, 0 }, 
+                { 0, 0, 2 } 
+            };
+             
+            Assert.AreEqual(2, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testOWinOnUpDiagonal() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 1, 1, 2 }, 
+                { 0, 2, 0 }, 
+                { 2, 0, 0 } 
+            };
+             
+            Assert.AreEqual(2, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testNoPlayBoard() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 0, 0, 0 }, 
+                { 0, 0, 0 }, 
+                { 0, 0, 0 } 
+            };
+             
+            Assert.AreEqual(-1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testNoWinAfter1TurnBoard() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 1, 0, 0 }, 
+                { 0, 0, 0 }, 
+                { 0, 0, 0 } 
+            };
+             
+            Assert.AreEqual(-1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testNoWinAfter2TurnBoard() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 1, 0, 0 }, 
+                { 0, 0, 0 }, 
+                { 0, 0, 2 } 
+            };
+             
+            Assert.AreEqual(-1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testNoWinAfter3TurnBoard() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 1, 1, 0 }, 
+                { 0, 0, 0 }, 
+                { 0, 0, 2 } 
+            };
+             
+            Assert.AreEqual(-1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testNoWinAfter4TurnBoard() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 1, 1, 0 }, 
+                { 0, 0, 2 }, 
+                { 0, 0, 2 } 
+            };
+             
+            Assert.AreEqual(-1, Kata.IsSolved(board));
+        }
+
+        [Test]
+        public void testDraw() 
+        {
+            int[,] board = new int[,] 
+            {
+                { 2, 1, 2 }, 
+                { 2, 1, 1 }, 
+                { 1, 2, 1 } 
+            };
+             
+            Assert.AreEqual(0, Kata.IsSolved(board));
         }
     }
 }
